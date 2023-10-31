072b2df16508a79478db98f5f3b13836d7fe6ac0
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-01 00:08:25 +0100

    fix: Enforce input valid state
    Before proceeding to any request, we now ensure that the input string is not null

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ const App = () => {
   }
 
   const generate = (prompt) => {
+    if(
+      prompt == null ||
+      prompt === undefined ||
+      prompt === ""){
+      alert("Empty");
+      return;
+    }
+
     console.log("Generating from prompt: " + prompt);
     setIsLoading(true);
     clearInput();
@@ -21,6 +29,14 @@ const App = () => {
   }
 
   const analyze = async (image_url) => {
+    if(
+      prompt == null ||
+      prompt === undefined ||
+      prompt === ""){
+      alert("Empty");
+      return;
+    }
+
     try {
       setImageSrc(image_url);
       setIsLoading(true);
@@ -37,15 +53,18 @@ const App = () => {
     <div className='page'>
       <h1>Computer Vision</h1>
       <p>Insert URL or type a prompt</p>
-      <input
-        id="prompt-input"
-        placeholder= "Enter URL to analyze or textual prompt to generate an image"
-        onChange={ e => setInputValue(e.target.value)}
-        type="text" />
-      <div className='buttons'>
-        <button onClick={ _ => generate(inputValue) }>Generate</button>
-        <button onClick={ async _ => await analyze(inputValue) }>Analyze</button>
-      </div>
+      <form onSubmit={ e => e.preventDefault() }>
+        <input
+          id="prompt-input"
+          placeholder= "Enter URL to analyze or textual prompt to generate an image"
+          onChange={ e => setInputValue(e.target.value)}
+          type="text"
+          required />
+        <div className='buttons'>
+          <button onClick={ _ => generate(inputValue) }>Generate</button>
+          <button onClick={ async _ => await analyze(inputValue) }>Analyze</button>
+        </div>
+      </form>
       {isLoading && <div>Loading...</div>}
       <img src={imageSrc} alt={imageDescription} />
       <div className="image-description">{imageDescription}</div>
