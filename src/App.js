4e2fa934d7b3a46fbbbfb36a6d05d767e953b9b1
Djoufson CHE BENE <djouflegran@gmail.com>
2023-10-31 19:20:56 +0100

    fix: Loading state
    When the application is processing the request, we set a nice `Loading ...` message to notify the user that something is being processing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [imageSrc, setImageSrc] = useState("");
   const [imageDescription, setImageDescription] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const clearInput = () => {
     const promptInput = document.getElementById("prompt-input");
@@ -14,19 +15,21 @@ const App = () => {
 
   const generate = (prompt) => {
     console.log("Generating from prompt: " + prompt);
+    setIsLoading(true);
     clearInput();
+    setIsLoading(false);
   }
 
   const analyze = async (image_url) => {
     try {
-      const description = await analyzeImage(image_url);
       setImageSrc(image_url);
+      setIsLoading(true);
+      const description = await analyzeImage(image_url);
       setImageDescription(description);
-
     } catch (error) {
       console.log('error', error);
     }
-
+    setIsLoading(false);
     clearInput();
   }
 
@@ -43,7 +46,7 @@ const App = () => {
         <button onClick={ _ => generate(inputValue) }>Generate</button>
         <button onClick={ async _ => await analyze(inputValue) }>Analyze</button>
       </div>
-
+      {isLoading && <div>Loading...</div>}
       <img src={imageSrc} alt={imageDescription} />
       <div className="image-description">{imageDescription}</div>
     </div>
